ef7636c9725ba0d62e9e798520510acd2e7bcf5b
Phoenix <anmolkhanna2003@gmail.com>
2024-07-05 22:18:33 +0530

    prefix sum exercise

---
+++ SubArraySumEqK/subArrSum.java
@@ -0,0 +1,22 @@
+//Runtime: 25ms!
+package SubArraySumEqK;
+
+import java.util.HashMap;
+
+public class subArrSum {
+
+    public static void main(String... args) {}
+
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int result = 0;
+        int preSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            preSum += nums[i];
+            result += map.getOrDefault(preSum - k, 0);
+            map.put(preSum, map.getOrDefault(preSum, 0) + 1);
+        }
+        return result;
+    }
+}
